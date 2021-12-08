ff356e78be4f8342b73ad9e5f575eb8cb7e31799
Andrei Ryan Taylor <andreirtaylor@gmail.com>
2021-12-08 02:28:36 -0500

    Add sample day

---
+++ SAMPLE_DAY/s2.js
@@ -0,0 +1,39 @@
+// see https://www.npmjs.com/package/datastructures-js
+import {
+  Stack,
+
+  Queue,
+
+  EnhancedSet,
+
+  LinkedList,
+  LinkedListNode,
+  DoublyLinkedList,
+  DoublyLinkedListNode,
+
+  MinHeap,
+  MaxHeap,
+  HeapNode, // interface
+
+  PriorityQueueOptions, // interface
+  PriorityQueueItem, // interface
+  MinPriorityQueue,
+  MaxPriorityQueue,
+
+  BinarySearchTree,
+  BinarySearchTreeNode,
+  AvlTree,
+  AvlTreeNode,
+
+  Trie,
+  TrieNode,
+
+  Graph,
+  DirectedGraph,
+} from 'datastructures-js';
+
+const fs = require('fs');
+
+
+  
+const data = fs.readFileSync('./testInput.txt', {encoding:'utf8', flag:'r'});
