c9697a91bc3acbe8586b63e906024b02cdb08655
Andrei Ryan Taylor <andreirtaylor@gmail.com>
2021-12-09 22:25:14 -0500

    2015 day 8

--- 2015/08/s1.js
+++ 2015/08/s1.js
@@ -2,21 +2,8 @@ const fs = require('fs');
 
 const data = fs.readFileSync('./testInput.txt', { encoding: 'utf8', flag: 'r' });
 
-let total = 0;
-data.trim().split("\n").forEach(line => {
-  const startLine = line
-  const start = line.length;
-  let real = 0;
-  console.log(line)
-  line = line
-    .replace(/(^\"|\"$)/g, "")
-
-    .replace(/\\x[a-f0-9][a-f0-9]/g, "X")
-    .replace(/\\"/g, "X")
-    .replace(/\\\\/g, "X")
-  console.log(line)
-  console.log(start, line.length)
-  total += start - line.length;
+data.split("\n").forEach(()=> {
+  const [start, end, dist] = line.split(/(to|=)/).map(x => x.trim())
+  console.log(start, end, dist)
 })
 
-console.log(total)
