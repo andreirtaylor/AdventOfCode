a06e38887b63c709678a0be37e8b3e9b5d098e3e
Andrei Ryan Taylor <andreirtaylor@gmail.com>
2021-12-15 15:51:04 -0500

    Add working code

--- 15/s2.js
+++ 15/s2.js
@@ -11,15 +11,7 @@ let cave = data
   .split('\n')
   .map(l => l.split('').map(risk => ({riskToMoveHere: Number.MAX_SAFE_INTEGER, risk: risk | 0})));
 
-for (let i = 0; i < cave.length; ++i) {
-  let line = '';
-  for (let j = 0; j < cave[0].length; ++j) {
-    line += cave[i][j].risk;
-  }
-  console.log(line);
-}
-console.log('');
-
+// extend each row with the incremented version along the x axis
 cave.forEach((row, r) => {
   const tmpRow = [...row];
   for (let i = 1; i < 5; ++i) {
@@ -27,8 +19,6 @@ cave.forEach((row, r) => {
       ...row.map(cell => {
         let tmp = {...cell};
         tmp.risk = (cell.risk + i) % 9 || 9;
-        // if (i === 4) console.log(tmp.risk, cell.risk + i, (cell.risk + i) % 10 || 1);
-
         return tmp;
       })
     );
@@ -36,16 +26,8 @@ cave.forEach((row, r) => {
   cave[r] = tmpRow;
 });
 
-for (let i = 0; i < cave.length; ++i) {
-  let line = '';
-  for (let j = 0; j < cave[0].length; ++j) {
-    line += cave[i][j].risk;
-  }
-  //console.log(line);
-}
-
+/// extend each row to a new row on the y axis
 const cpy = [...cave];
-
 for (let i = 1; i < 5; ++i) {
   cave.forEach(row => {
     cpy.push(
@@ -57,28 +39,18 @@ for (let i = 1; i < 5; ++i) {
     );
   });
 }
-
 cave = cpy;
-
-for (let i = 0; i < cave.length; ++i) {
-  let line = '';
-  for (let j = 0; j < cave[0].length; ++j) {
-    line += cave[i][j].risk;
-  }
-  console.log(line);
-}
 
 let endX = cave.length - 1;
 let endY = cave[0].length - 1;
 
 const q = new MinPriorityQueue({
-  compare: (a, b) => {
-    if (a.risk < b.risk) return -1; // do not swap
-    if (a.risk > b.risk) return 1; // swap
-
-    // salaries are the same, compare rank
-    return a.risk > b.risk ? 1 : -1;
-  },
+  // compare: (a, b) => {
+  //   if (a.risk < b.risk) return -1; // do not swap
+  //   if (a.risk > b.risk) return 1; // swap
+  //   // salaries are the same, compare rank
+  //   return a.risk > b.risk ? 1 : -1;
+  // },
 });
 
 q.enqueue({
